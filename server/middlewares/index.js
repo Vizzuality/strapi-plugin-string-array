62907b540d2ecd4661d9db319e20b0bf67bb2c6a
Tiago Garcia <tiagojsag@gmail.com>
2024-02-12 10:04:47 +0100

    wip

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,19 +1,38 @@
 'use strict';
 
 const _ = require('lodash');
-const { getPluginService } = require('../util/getPluginService');
 
-const transform = async (strapi, ctx, next) => {
+const separatorMap = {
+  'comma': ',',
+  'semicolon': ';',
+  'pipe': '|',
+}
+
+const transform = async (fieldMap, strapi, ctx, next) => {
   await next();
 
   // ensure body exists, occurs on non existent route
-  if (!ctx.body) {
+  if (!ctx.body || !ctx.body.data) {
     return;
   }
 
-  // ensure no error returned.
-  if (!ctx.body.data) {
-    return;
+  for (const [key, value] of Object.entries(fieldMap)) {
+    switch (true) {
+      case ctx.response.body.data instanceof Object:
+        if (ctx.response.body.data.attributes[key]) {
+          ctx.response.body.data.attributes[key] = ctx.response.body.data.attributes[key].split(separatorMap[value.options.separator || 'comma'] );
+        }
+        break;
+      case ctx.response.body.data instanceof Array:
+        ctx.response.body.data = ctx.response.body.data.map((item) => {
+          if (item.attributes[key]) {
+            item.attributes[key] = item.attributes[key].split(separatorMap[value.options.separator || 'comma'] );
+          }
+        })
+        break;
+      default:
+        return;
+    }
   }
 };
 
